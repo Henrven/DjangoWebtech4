d2b73469eece84223d234c2f2e0212601a7df162
Rovervdven <45846402+Rovervdven@users.noreply.github.com>
2019-03-25 16:54:14 +0100

    load hours

--- Urentot/views.py
+++ Urentot/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import End
+from datetime import datetime
 
 def index(request):
     
-    exit = End.date_now.value_to_string
+    now = End.date_now
+    exit = End.date_exit
     
     context = {
+        'now' : now,
         'exit': exit
     }
     return render(request, 'detail.html', context)
